7b99a5ca7222fe745b11d9c676902106596bf6d4
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2017-07-20 15:06:43 +0100

    Create initial 'add host' form

--- src/cloudlabs/views_host.py
+++ src/cloudlabs/views_host.py
@@ -1,9 +1,16 @@
 
 from flask import (
     Blueprint,
+    flash,
+    g,
+    redirect,
     render_template,
+    request,
+    url_for,
 )
+from .forms.add_host import AddHostForm
 from .models import Host, SshKey, User
+from .utils import login_required
 
 
 blueprint = Blueprint('host', __name__)
@@ -14,9 +21,27 @@ def info(id):
     return ''
 
 
-@blueprint.route('/host/add')
+@blueprint.route('/host/add', methods=('GET', 'POST'))
+@login_required
 def add():
-    return ''
+    form = AddHostForm()
+    # Fill in options for SSH key
+    form.admin_ssh_key.choices = [(key.id, key.label) for key in g.user.ssh_keys]
+    if form.validate_on_submit():
+        fields = {
+            'user_id': g.user.id,
+        }
+        for field in ['label', 'dns_name', 'description', 'admin_username']:
+            fields[field] = form[field].data.strip()
+        if form.auth_type.data == 'SSH':
+            fields['admin_ssh_key_id'] = form.admin_ssh_key.data
+        else:
+            fields['admin_password'] = form.admin_password.data
+        # TODO: Creation script in fake Terraform state
+        Host.create(**fields)
+        flash('Host "{}" added'.format(form.label.data), 'success')
+        return redirect(url_for('main.index'))
+    return render_template('add_host.html', form=form)
 
 
 @blueprint.route('/host/<int:id>/edit')
