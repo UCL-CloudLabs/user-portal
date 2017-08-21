89166c9c4a9e310ca2e170530d0bdca9b7a8dc0d
Jonathan Cooper <j.p.cooper@ucl.ac.uk>
2017-08-21 09:18:21 +0100

    Allow secrets to be set by the WSGI launch script
    Reorganised a couple between prod/dev settings in the process.

--- src/cloudlabs/config.py
+++ src/cloudlabs/config.py
@@ -8,7 +8,7 @@ class Config:
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'change-this'
+    SECRET_KEY = os.environ.get('CLOUDLABS_SECRET_KEY')
     SSO_ATTRIBUTE_MAP = {
         'eppn': (True, 'eppn'),
         'displayName': (True, 'name'),
@@ -20,12 +20,16 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',
                                              'postgresql:///cloudlabs')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # Default to travis config
-    PRIVATE_SSH_KEY_PATH = os.environ.get(
-            'PRIVATE_SSH_KEY_PATH',
-            '~/build/UCL-CloudLabs/user-portal/src/test/id_rsa_travis_azure')
+    # Require this to be set explicitly
+    PRIVATE_SSH_KEY_PATH = os.environ.get('PRIVATE_SSH_KEY_PATH')
 
 
 class DevConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    # Default to an insecure secret key for local development
+    SECRET_KEY = os.environ.get('CLOUDLABS_SECRET_KEY', 'change-this!')
+    # Default to travis config
+    PRIVATE_SSH_KEY_PATH = os.environ.get(
+        'PRIVATE_SSH_KEY_PATH',
+        '~/build/UCL-CloudLabs/user-portal/src/test/id_rsa_travis_azure')
