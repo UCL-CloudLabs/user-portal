c08b106aa6a5ffd4f23bb2d4e1c179fd59e29aa9
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2017-10-17 16:46:43 +0100

    Add placeholder for getting status

--- src/cloudlabs/host_status.py
+++ src/cloudlabs/host_status.py
@@ -10,3 +10,12 @@ class HostStatus(Enum):
     stopped = 'Stopped'      # Machine deployed but not turned on
     destroying = 'Destroying'
     error = 'Failed to deploy'
+
+def get_status_azure(host):
+    """Return the status of a host deployed on Azure."""
+    # TODO Get the name of the VM and resource group, and use the ComputeManager
+    # to retrieve the status. This is a placeholder while the naming becomes
+    # stable.
+    # Something like this:
+    #stat = cm.virtual_machines.get(vm_name, group_name).instance_view.statuses
+    return HostStatus.error
