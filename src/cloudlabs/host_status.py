db750d57896fdf0df0bdffa12d245f45a23bda64
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2017-12-04 16:40:08 +0000

    Finish a rough version of getting status

--- src/cloudlabs/host_status.py
+++ src/cloudlabs/host_status.py
@@ -1,5 +1,10 @@
 from enum import Enum
 
+from azure.common.credentials import ServicePrincipalCredentials
+from azure.mgmt.compute import ComputeManagementClient
+
+from . import names
+from .secrets import Secrets
 
 class HostStatus(Enum):
     """Possible statuses for CloudLabs hosts."""
@@ -12,36 +17,76 @@ class HostStatus(Enum):
     destroying = 'Destroying'
     error = 'Failed to deploy'
 
+
 def get_status_azure(host):
     """Return the status of a host deployed on Azure."""
     # Getting the status is not obvious at first glance. This has some info:
     # https://docs.microsoft.com/en-us/rest/api/compute/virtualmachines/virtualmachines-state
-    # TODO Get the name of the VM and resource group, and use the ComputeManager
-    # to retrieve the status. This is a placeholder while the naming becomes
-    # stable.
-    # Something like this:
-    statuses = cm.virtual_machines.instance_view(group_name, vm_name).statuses
-    prov_status = get_provisioning_status(host)
-    if prov_status == "deleting":
+    # TODO Avoid creating this every time (eg by passing cmc as an argument)
+    credentials, subscription_id = get_credentials()
+    cmc = ComputeManagementClient(credentials, subscription_id)
+    try:
+        group_name = names.group_name(host)
+        vm_name = names.vm_name(host)
+        print(group_name, vm_name)
+        # The InstanceViewStatus object returned has a code attribute and a
+        # slightly more human-readable display_status. The code seems to follow
+        # a particular structure, so it's probably best to work with that.
+        statuses = [
+            status.code
+            for status
+            in cmc.virtual_machines.instance_view(group_name, vm_name).statuses
+        ]
+        print(statuses)
+    except Exception as e:
+        return HostStatus.error
+    prov_state = get_provisioning_state_azure(statuses)
+    if prov_state == "deleting":
         return HostStatus.destroying
     else:
         for status in statuses:
-            power_status = _remove_prefix(status, "PowerStatus/")
-            if power_status == "deallocated" or power_status == "stopped":
+            # Look for the (one?) status which holds the PowerState. This would
+            # be nicer if we could guarantee that there are always exactly two
+            # statuses (provisioning and power), but I'm not sure whether that
+            # is true.
+            if not status.startswith("PowerState"):
+                continue
+            power_state = _remove_prefix(status, "PowerState/")
+            if power_state == "deallocated" or power_state == "stopped":
                 return HostStatus.stopped
-            elif power_status == "starting":
+            elif power_state == "starting":
                 return HostStatus.starting
-            elif power_status == "running":
+            elif power_state == "running":
                 return HostStatus.running
-            elif power_status == "deallocating" or power_status == "stopping":
+            elif power_state == "deallocating" or power_state == "stopping":
                 return HostStatus.stopping
         return HostStatus.error #  uknown status, return error
 
 
-def get_provisioning_status_azure(host):
-    #statuses = cm.virtual_machines.instance_view(group_name, vm_name).statuses
+def get_credentials():
+    """Get the necessary credentials for creating the ComputeManagementClient.
+
+    Also see: https://github.com/Azure-Samples/virtual-machines-python-manage
+    """
+    # TODO are secrets better accessible in a different way?
+    # subscription_id = os.environ['TF_VAR_azure_subscription_id']
+    # credentials = ServicePrincipalCredentials(
+    #     client_id=os.environ['TF_VAR_azure_client_id'],
+    #     secret=os.environ['TF_var_azure_client_secret'],
+    #     tenant=os.environ['TF_var_azure_tenant_id']
+    # )
+    subscription_id = Secrets.TF_VAR_azure_subscription_id
+    credentials = ServicePrincipalCredentials(
+        client_id=Secrets.TF_VAR_azure_client_id,
+        secret=Secrets.TF_VAR_azure_client_secret,
+        tenant=Secrets.TF_VAR_azure_tenant_id
+    )
+    return credentials, subscription_id
+
+
+def get_provisioning_state_azure(statuses):
     for status in statuses:
-        if status.code.startswith("ProvisioningState/"):
+        if status.startswith("ProvisioningState/"):
             return _remove_prefix(status, "ProvisioningState/")
     assert False  # should never happen
     return None
