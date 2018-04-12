331728132bf4ad6e3d46d59c2deabad17c3dbaf7
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2018-04-12 14:09:15 +0100

    Add incomplete test for status sequences

--- src/test/test_azure_tools.py
+++ src/test/test_azure_tools.py
@@ -1,6 +1,6 @@
 import pytest
 
-from cloudlabs.azure_tools import _parse_statuses
+from cloudlabs.azure_tools import AzureTools, _parse_statuses
 from cloudlabs.host_status import HostStatus
 
 # Example of status codes returned by the Azure API:
@@ -29,3 +29,38 @@ output_statuses = [
 def test_parse_statuses(statuses, result):
     """Test that a VM's status as retrieved from Azure is correctly interpreted."""
     assert _parse_statuses(statuses) == result
+
+
+@pytest.fixture(scope="module")
+def host():
+    """Mock a DB entry (Host) to use with AzureTools."""
+    class MockHost():
+        def __init__(self, dns_name):
+            self.dns_name = dns_name
+    return MockHost("")
+
+
+@pytest.fixture
+def image():
+    # create the machine
+    vm = None
+
+    def tear_down():
+        pass
+    vm.addfinalizer(tear_down)
+    return vm
+
+
+def test_azure_action_sequence(image, host):
+    """Test that the AzureTools methods drive a VM through the right states."""
+    # Set up test by starting an existing VM
+    tools = AzureTools()
+    tools.start_VM(host)
+    assert tools.get_status(host) == HostStatus.running, \
+        "Host status should be running"
+    tools.restart_VM(host)
+    assert tools.get_status(host) == HostStatus.running, \
+        "Host status should be running"
+    tools.stop_VM(host)
+    assert tools.get_status(host) == HostStatus.stopped, \
+        "Host status should be stopped"
